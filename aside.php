6d315ce369247d3d319f1359b2425fdcf1c30689
emmanuelvelanganni <emmanuevelanganni@gmail.com>
2015-05-11 16:52:09 +0530

    add

---
+++ aside.php
@@ -0,0 +1,64 @@
+			<section class="main-section">
+				<div id="aside-links-block">
+					<div id="xsysys-news-links" class="xsysys-links">
+						<h1>
+							Latest News
+						</h1>
+						<ul>
+							<li>
+								<a href="#"><span class="glyphicon glyphicon-stop"></span>&nbsp;&nbsp;News1</a>
+							</li>
+							<li>
+								<a href="#"><span class="glyphicon glyphicon-stop"></span>&nbsp;&nbsp;News2</a>
+							</li>
+							<li>
+								<a href="#"><span class="glyphicon glyphicon-stop"></span>&nbsp;&nbsp;News3</a>
+							</li>
+							<li>
+								<a href="#"><span class="glyphicon glyphicon-stop"></span>&nbsp;&nbsp;News4</a>
+							</li>
+							<li>
+								<a href="#"><span class="glyphicon glyphicon-stop"></span>&nbsp;&nbsp;News5</a>
+							</li>
+							<li>
+								<a href="#"><span class="glyphicon glyphicon-stop"></span>&nbsp;&nbsp;News6</a>
+							</li>				
+							<li>
+								<a href="#"><span class="glyphicon glyphicon-stop"></span>&nbsp;&nbsp;News7</a>
+							</li>					
+						</ul>
+					</div>
+					<div id="xsysys-media-links" class="xsysys-links">
+						<h1>
+							Social Media
+						</h1>
+						<ul>
+							<li>
+								<a href="#"><span class="glyphicon glyphicon-stop"></span>&nbsp;&nbsp;Facebook</a>
+							</li>
+							<li>
+								<a href="#"><span class="glyphicon glyphicon-stop"></span>&nbsp;&nbsp;Twitter</a>
+							</li>
+							<li>
+								<a href="#"><span class="glyphicon glyphicon-stop"></span>&nbsp;&nbsp;Linked In</a>
+							</li>
+							<li>
+								<a href="#"><span class="glyphicon glyphicon-stop"></span>&nbsp;&nbsp;Delicious</a>
+							</li>
+							<li>
+								<a href="#"><span class="glyphicon glyphicon-stop"></span>&nbsp;&nbsp;Digg</a>
+							</li>
+							<li>
+								<a href="#"><span class="glyphicon glyphicon-stop"></span>&nbsp;&nbsp;StumbleUpon</a>
+							</li>				
+							<li>
+								<a href="#"><span class="glyphicon glyphicon-stop"></span>&nbsp;&nbsp;Tumblr</a>
+							</li>					
+						</ul>
+					</div>
+				</div>
+			</section>
+			<div id="aside-toggle-btn-block">
+				<a id="aside-toggle-btn" class="aside-btn-right" onclick="toggleAside(this.id)"><img src="images/xsysys-ui/aside-toggle/aside-toggle-right.png" alt="aside-btn"/></a>
+				<div class="clearboth"></div>
+			</div>
