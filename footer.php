35bf9c5e9d47aa8919cb8a9c0f843547673b3405
emmanuelvelanganni <emmanuevelanganni@gmail.com>
2015-05-11 16:47:38 +0530

    add

---
+++ footer.php
@@ -0,0 +1,17 @@
+			<div class="links">
+				<ul>
+					<li>
+						<a>Feedback</a>
+					</li>
+					<li>
+						<a>Contact Us</a>
+					</li>
+					<li>
+						<a>Help</a>
+					</li>
+				</ul>
+			</div>
+			<div class="copyrights">
+				<span>Copyright &copy; 2015 - <span class="xsysys-style">xsysys</span> Technologies Pvt. Ltd. All Rights Reserved.</span>
+			</div>
+		
