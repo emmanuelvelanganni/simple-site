e8fc3efb37f380e12f6fb97d245635d76f1434b7
emmanuelvelanganni <emmanuevelanganni@gmail.com>
2015-05-12 13:13:53 +0530

    add

---
+++ aboutus-pages/aboutus-main-content.php
@@ -0,0 +1,103 @@
+						<div class="clearboth"></div>
+						<div class="xsysys-content-row row1">
+							<div class="xsysys-box no-left-right-border">
+								<section class="xsysys-box-content">
+									<h3>
+										<img src="images/xsysys-articles/aboutus/history.png"/>&nbsp;&nbsp;History
+									</h3>
+									<p>
+										The company was established in 1996 as an offshore development center for the parent company in Belgium...
+									</p>
+									<a id="history-link" onclick="loadHistory()"><span class="glyphicon glyphicon-hand-right"></span>&nbsp;&nbsp;Discover</a>	
+								</section>	
+							</div>
+							<div class="xsysys-box">
+								<section class="xsysys-box-content">
+									<h3>
+										<img src="images/xsysys-articles/aboutus/vmission.png"/>&nbsp;&nbsp;Vision & Mission
+									</h3>
+									<p>
+										We believe that life on our planet and therefore in the universe is about evolution, it is about survival within...
+									</p>
+									<a id="vmision-link" onclick="loadVisionMission()"><span class="glyphicon glyphicon-hand-right"></span>&nbsp;&nbsp;Discover</a>	
+								</section>	
+							</div>
+							<div class="xsysys-box no-left-right-border">
+								<section class="xsysys-box-content">
+									<h3>
+										<img src="images/xsysys-articles/aboutus/technology.png"/>&nbsp;&nbsp;Technology
+									</h3>
+									<p>
+										Technology leadership is a critical driver for hi-tech companies. New market entrants rely on technology leadership...
+									</p>
+									<a id="technology-link" onclick="loadTechnology()"><span class="glyphicon glyphicon-hand-right"></span>&nbsp;&nbsp;Discover</a>
+								</section>	
+							</div>
+						</div>
+						<div class="clearboth"></div>
+						<div class="xsysys-content-row row2">
+							<div class="xsysys-box no-left-right-border">
+								<section class="xsysys-box-content">
+									<h3>
+										<img src="images/xsysys-articles/aboutus/team.png"/>&nbsp;&nbsp;Team
+									</h3>
+									<p>
+										Day by day our Indo - European management team inspires an exceptional group of people delivering...
+									</p>
+									<a id="team-link" onclick="loadTeam()"><span class="glyphicon glyphicon-hand-right"></span>&nbsp;&nbsp;Discover</a>	
+								</section>	
+							</div>
+							<div class="xsysys-box">
+								<section class="xsysys-box-content">
+									<img src="images/xsysys-asset/xsysys-logo-180.png"/>
+								</section>	
+							</div>
+							<div class="xsysys-box no-left-right-border">
+								<section class="xsysys-box-content">
+									<h3>
+										<img src="images/xsysys-articles/aboutus/brand.png"/>&nbsp;&nbsp;Brand
+									</h3>
+									<p>
+										Our brand represents LIFE, a journey through space. It is all about survival and when doing so learning...
+									</p>
+									<a id="brand-link" onclick="loadBrand()"><span class="glyphicon glyphicon-hand-right"></span>&nbsp;&nbsp;Discover</a>	
+								</section>	
+							</div>
+						</div>
+						<div class="clearboth"></div>
+						<div class="xsysys-content-row row3">
+							<div class="xsysys-box no-left-right-border">
+								<section class="xsysys-box-content">
+									<h3>
+										<img src="images/xsysys-articles/aboutus/gateway.png"/>&nbsp;&nbsp;Gateway
+									</h3>
+									<p>
+										BELINDA<sup>TM</sup> is our IT-Services gateway between India and Belgium...
+									</p>
+									<a id="gateway-link" onclick="loadGateway()"><span class="glyphicon glyphicon-hand-right"></span>&nbsp;&nbsp;Discover</a>	
+								</section>	
+							</div>
+							<div class="xsysys-box">
+								<section class="xsysys-box-content">
+									<h3>
+										<img src="images/xsysys-articles/aboutus/news.png"/>&nbsp;&nbsp;News
+									</h3>
+									<p>
+										HRH Prince Philippe of Belgium, leading a high level Belgian Trade Mission to India...
+									</p>
+									<a id="news-link" onclick="loadNews()"><span class="glyphicon glyphicon-hand-right"></span>&nbsp;&nbsp;Discover</a>	
+								</section>	
+							</div>
+							<div class="xsysys-box no-left-right-border">
+								<section class="xsysys-box-content">
+									<h3>
+										<img src="images/xsysys-articles/aboutus/qualitysystems.png"/>&nbsp;&nbsp;Quality Systems
+									</h3>
+									<p>
+										The quality systems at <span class="xsysys_style">xsysys</span> match the best in the  world. We benchmark ourselves against international...
+									</p>
+									<a id="qsystems-link" onclick="loadQualitySystems()"><span class="glyphicon glyphicon-hand-right"></span>&nbsp;&nbsp;Discover</a>	
+								</section>	
+							</div>
+						</div>
+						<div class="clearboth"></div>
