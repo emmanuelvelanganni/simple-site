536371603f41ee7b1b53b583b91ba4a123c534ab
emmanuelvelanganni <emmanuevelanganni@gmail.com>
2015-05-22 17:04:03 +0530

    add

--- aboutus.php
+++ aboutus.php
@@ -1,47 +1,15 @@
-<!doctype html>
-<html>
-	<head>
-		<?php
-		$page_title = 'index.php';
-		$pgDesc = 'Main Page';
-		$keyWords = 'xsysys website';
-		include ('head.php');
-		?>
-		<script>
-			$(document).ready(function(){
-				$('#page-navigator ul li').find('#aboutus').addClass('active');
-			});
-		</script>
-	</head>
-	<body>
-		<div id="main-aboubus" class="main">
-			<?php
-				include ('header.php');
-			?>
-			<article>
-				<section id="aboutus-main-section" class="main-section">
-					<div class="xsysys-aboutus-content-block" id="xsysys-aboutus-content-block">
-						<script>
-							$(document).ready(function(){
-								$('#xsysys-aboutus-content-block').load('aboutus-pages/aboutus-main-content.php');
-							});
-						</script>
-					</div>
-				</section>
-			</article>
-			<aside>
-				<?php
-					include('aside.php');
-				?> 
-			</aside>
-			<footer>
-				<?php
-					include('footer.php');
-				?> 
-			</footer>
-		</div>
-	</body>
-</html>
+<script>
+	$(document).ready(function(){
+		$('#page-navigator ul li').find('#aboutus').addClass('active');
+	});
+</script>
+<div class="xsysys-aboutus-content-block" id="xsysys-aboutus-content-block">
+	<script>
+		$(document).ready(function(){
+			$('#xsysys-aboutus-content-block').load('aboutus-pages/aboutus-main-content.php');
+		});
+	</script>
+</div>
 <script>
 	function load_internalnews(){
 		$("#session_body").unload("aboutus-pages/pressreleases.php");
